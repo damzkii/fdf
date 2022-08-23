90752c850061edf546ac49b18a97ba60ef62f8f8
Adam Hermawan <ahermawa@c3r5p5.hive.fi>
2022-08-23 14:26:56 +0300

    slight edits

--- src/read_map.c
+++ src/read_map.c
@@ -6,24 +6,37 @@
 /*   By: ahermawa <ahermawa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/15 17:38:57 by ahermawa          #+#    #+#             */
-/*   Updated: 2022/08/22 19:18:00 by ahermawa         ###   ########.fr       */
+/*   Updated: 2022/08/23 14:25:40 by ahermawa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static void map_rows_columns(char *file_name, t_data *data, int rows)
+#include "fdf.h"
+
+static void get_cols_rows(char *filename, t_data *data, int rows)
 {
     char    *line;
     char    **tmp;
+    int     fd;
     int     i;
+
+    i = 0
+    fd = open(filename, O_RDONLY);
+    if (!(get_next_line(filename, &line))
+        err_msg(1, "Error!");
+    tmp = ft_strsplit(line, ' ');
+    while (tmp[i])
+        i++;
+    data->map.cols = i - 1;
+    
     
     
 }
-void    read_map(char *file_name)
+void    read_map(char *filename)
 {
     int     fd;
     int     j;
 
-    map_rows_columns(file_name, data, 0);
+    get_cols_rows(filename, data, 0);
     
     
 }
