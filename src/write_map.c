b96eac9d74be54819e5e14f2da8c188f01b53594
Adam Hermawan <ahermawa@c1r6p14.hive.fi>
2022-09-08 16:24:15 +0300

    started writing map cols part

--- src/write_map.c
+++ src/write_map.c
@@ -6,26 +6,29 @@
 /*   By: ahermawa <ahermawa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 09:35:26 by ahermawa          #+#    #+#             */
-/*   Updated: 2022/09/07 14:41:20 by ahermawa         ###   ########.fr       */
+/*   Updated: 2022/09/08 16:23:29 by ahermawa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
+static void fill_cols(t_data *data, double *px, double *py)
+{
+    px[0] = data->offset + (data->i * data->gap);
+    px[1] = data->offset + (data->i * data->gap) + data-gap;
+}
 void    draw_map(t_data *data)
 {
     double  px[3];
     double  py[3];
-    int     i;
-    int     j;
     
-    j = -1;
-    while (++j < data->map.rows - 1)
+    data->j = -1;
+    while (++data->j < data->map.rows - 1)
     {
-        i = -1;
-        while (++i < data->map.cols)
+        data->i = -1;
+        while (++data->i < data->map.cols)
         {
-            
+            fill_cols(data, px, py);
         }
     }    
 }
